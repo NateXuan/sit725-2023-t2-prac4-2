b7a24aa03c2fb2f14a87bfd7370a11123ba6f056
NateXuan <natexuan0805@gmail.com>
2023-08-01 18:35:04 +1000

    1

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
-const cardList = [
-    {
-        title: "Kitten 2",
-        image: "kitten-2.jpg",
-        link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
-    },
-    {
-        title: "Kitten 3",
-        image: "kitten-3.webp",
-        link: "About Kitten 3",
-        desciption: "This is a yellow cat"
-    }
-]
-
 const addCards = (items) => {
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">' +
-            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.path + '">' +
             '</div><div class="card-content">' +
-            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="https://en.wikipedia.org/wiki/Cat">' + item.link + '</a></p></div>' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="https://en.wikipedia.org/wiki/Cat">' + item.subTitle + '</a></p></div>' +
             '<div class="card-reveal">' +
             '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
-            '<p class="card-text">' + item.desciption + '</p>' +
-            '</div></div></div>';
+            '<p class="card-text">' + item.description + '</p>' + '<button class="delete-button btn waves-effect waves-light" data-id="' + item._id + '">Delete</button>'
+        '</div></div></div>';
         $("#card-section").append(itemToAppend)
     });
 }
 
 const submitForm = () => {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.subTitle = $('#subTitle').val();
+    formData.path = $('#path').val();
+    formData.description = $('#description').val();
     console.log("Form Data Submitted: ", formData);
+    postCat(formData);
+}
+
+function postCat(cat) {
+    $.ajax({
+        url: '/api/cat',
+        type: 'POST',
+        data: cat,
+        success: (result) => {
+            if (result.statusCode === 200) {
+                alert('cat added');
+                getAllCats();
+            }
+        }
+    })
+}
+
+function getAllCats() {
+    $("#card-section").empty();
+    $.get('/api/cats', (result) => {
+        if (result.statusCode === 200) {
+            addCards(result.data);
+        }
+    })
 }
 
 $(document).ready(function () {
@@ -41,6 +50,25 @@ $(document).ready(function () {
     $('#formSubmit').click(() => {
         submitForm();
     })
-    addCards(cardList);
     $('.modal').modal();
+    getAllCats();
 });
+
+$(document).on('click', '.delete-button', function () {
+    let id = $(this).data('id');
+    deleteCat(id);
+});
+
+
+function deleteCat(id) {
+    $.ajax({
+        url: '/api/cat/' + id,
+        type: 'DELETE',
+        success: (result) => {
+            if (result.statusCode === 200) {
+                alert('cat deleted');
+                getAllCats();
+            }
+        }
+    })
+}
